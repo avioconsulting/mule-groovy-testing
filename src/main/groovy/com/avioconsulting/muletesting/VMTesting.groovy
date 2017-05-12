18475ef78ad6a536082d6b5ffcd60851c35a762d
Brady Wied <brady@wied.us>
2017-05-12 10:36:45 -0600

    Add more TODOs

--- src/main/groovy/com/avioconsulting/muletesting/VMTesting.groovy
+++ src/main/groovy/com/avioconsulting/muletesting/VMTesting.groovy
@@ -5,6 +5,8 @@ import com.avioconsulting.muletesting.transformers.YieldType
 import org.mule.munit.common.mocking.Attribute
 import org.mule.munit.common.mocking.MessageProcessorMocker
 
+// TODO: pull JSON out of this and allow choosing? OR pull VM out and make traits more about format
+// then separate traits to get the MessageProcessorMocker object
 trait VMTesting {
     def MessageProcessorMocker mockQueueReceive(String name, Class expectedRequestJsonClass, YieldType yieldType = YieldType.Map, testClosure) {
         def mock = getVmqReceive(name)
