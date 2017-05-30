149e70c9233d99692001bf616932fe104a5e0399
Brady Wied <brady@wied.us>
2017-05-30 17:02:16 -0600

    Extract out XML message logic

--- src/main/groovy/com/avioconsulting/mule/testing/transformers/xml/XMLTransformer.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/transformers/xml/XMLTransformer.groovy
@@ -1,26 +1,22 @@
 package com.avioconsulting.mule.testing.transformers.xml
 
-import org.apache.cxf.staxutils.DepthXMLStreamReader
-import org.mule.DefaultMuleMessage
 import org.mule.api.MuleContext
 import org.mule.api.MuleMessage
 import org.mule.modules.interceptor.processors.MuleMessageTransformer
 
 import javax.xml.bind.JAXBElement
-import javax.xml.stream.XMLInputFactory
 
 class XMLTransformer implements MuleMessageTransformer {
     private final Closure closure
-    private final MuleContext muleContext
     private final JAXBMarshalHelper helper
+    private final XMLMessageBuilder builder
 
     XMLTransformer(Closure closure,
                    MuleContext muleContext,
                    Class inputJaxbClass) {
-
-        this.muleContext = muleContext
         this.closure = closure
         this.helper = new JAXBMarshalHelper(inputJaxbClass)
+        this.builder = new XMLMessageBuilder(muleContext)
     }
 
     MuleMessage transform(MuleMessage incomingMessage) {
@@ -35,44 +31,7 @@ class XMLTransformer implements MuleMessageTransformer {
         }
         def reply = this.closure(strongTypedPayload)
         assert reply instanceof JAXBElement
-        getXmlMessageFromJaxbElement(reply,
-                                     200)
-    }
-
-    private MuleMessage getXmlMessageFromJaxbElement(JAXBElement jaxbElement,
-                                                     Integer httpStatus) {
-        def reader = helper.getMarshalled(jaxbElement)
-        getXmlMessage reader, httpStatus
-    }
-
-    // incoming messages will not have a status
-    private MuleMessage getXmlMessage(readerOrStream,
-                                      Integer httpStatus = null) {
-        def payload = getNormalXmlPayload(readerOrStream)
-        constructXmlTypeMessage(httpStatus, payload)
-    }
-
-    private static getNormalXmlPayload(readerOrStream) {
-        def factory = XMLInputFactory.newInstance()
-        def xmlReader = factory.createXMLStreamReader readerOrStream
-        new DepthXMLStreamReader(xmlReader)
-    }
-
-    private MuleMessage constructXmlTypeMessage(Integer httpStatus,
-                                                payload) {
-        // need some of these props for SOAP mock to work properly
-        def messageProps = [
-                'content-type': 'text/xml; charset=utf-8'
-        ]
-        if (httpStatus != null) {
-            messageProps['http.status'] = httpStatus
-        }
-        def outboundProps = null
-        def attachments = null
-        new DefaultMuleMessage(payload,
-                               messageProps,
-                               outboundProps,
-                               attachments,
-                               this.muleContext)
+        def reader = helper.getMarshalled(reply)
+        this.builder.build(reader, 200)
     }
 }
