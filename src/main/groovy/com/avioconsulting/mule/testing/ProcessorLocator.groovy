d5931c0a213dfd5a5149c688a5a80aadaa5da2d0
Brady Wied <brady@wied.us>
2017-06-07 14:50:21 -0500

    Recursively locate processors

--- src/main/groovy/com/avioconsulting/mule/testing/ProcessorLocator.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/ProcessorLocator.groovy
@@ -3,6 +3,8 @@ package com.avioconsulting.mule.testing
 import org.mule.api.AnnotatedObject
 import org.mule.api.MuleEvent
 import org.mule.api.processor.MessageProcessor
+import org.mule.api.processor.MessageProcessorChain
+import org.mule.processor.AbstractMessageProcessorOwner
 
 import javax.xml.namespace.QName
 
@@ -16,15 +18,35 @@ class ProcessorLocator {
 
     def getProcessor(MuleEvent muleEvent) {
         def processors = muleEvent.flowConstruct.messageProcessors as List<MessageProcessor>
-        def processor = processors.find { processor ->
+        def allProcessors = recursiveProcessorList(processors)
+        def processor = findProcessor(allProcessors)
+        assert processor: "Unable to find processor with name '${processorName}'. Is doc:name present on the connector?"
+        processor
+    }
+
+    private def findProcessor(List<MessageProcessor> processors) {
+        processors.find { processor ->
             def annotated = processor as AnnotatedObject
             if (!annotated) {
-                return false
+                return
             }
             def name = annotated.annotations[new QName(doc, 'name')]
             name == processorName
         }
-        assert processor: "Unable to find processor with name '${processorName}'. Is doc:name present on the connector?"
-        processor
+    }
+
+    private List<MessageProcessor> recursiveProcessorList(List<MessageProcessor> input) {
+        input.collect { MessageProcessor processor ->
+            if (processor instanceof MessageProcessorChain) {
+                def processorList = processor.messageProcessors
+                return recursiveProcessorList(processorList)
+            }
+            // if it's inside an enricher, we need to search
+            if (processor instanceof AbstractMessageProcessorOwner) {
+                def processorList = processor.ownedMessageProcessors
+                return recursiveProcessorList(processorList)
+            }
+            processor
+        }.flatten() as List<MessageProcessor>
     }
 }
