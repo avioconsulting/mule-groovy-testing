8c239ccd9f8b2c2c5c2da966dbe7b05bf7b89fb7
Brady Wied <brady@wied.us>
2017-07-27 14:50:07 -0600

    Extract a reusable subclass

--- src/main/groovy/com/avioconsulting/mule/testing/transformers/sfdc/QueryTransformerSpy.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/transformers/sfdc/QueryTransformerSpy.groovy
@@ -1,32 +1,24 @@
 package com.avioconsulting.mule.testing.transformers.sfdc
 
 import com.avioconsulting.mule.testing.ProcessorLocator
+import com.avioconsulting.mule.testing.spies.DsqlBasedSpy
 import org.mule.DefaultMuleMessage
 import org.mule.api.MuleContext
-import org.mule.api.MuleEvent
-import org.mule.api.MuleException
 import org.mule.api.MuleMessage
-import org.mule.devkit.internal.dsql.DsqlMelParserUtils
 import org.mule.modules.interceptor.processors.MuleMessageTransformer
-import org.mule.modules.salesforce.generated.processors.QueryMessageProcessorDebuggable
-import org.mule.munit.common.mocking.SpyProcess
 
-class QueryTransformerSpy implements MuleMessageTransformer, SpyProcess {
-    private final ProcessorLocator locator
-    private final MuleContext muleContext
-    private String dsqlQuery
+class QueryTransformerSpy extends DsqlBasedSpy implements MuleMessageTransformer {
     private final Closure closure
 
     QueryTransformerSpy(ProcessorLocator locator,
                         MuleContext muleContext,
                         Closure closure) {
+        super(locator, muleContext)
         this.closure = closure
-        this.muleContext = muleContext
-        this.locator = locator
     }
 
     MuleMessage transform(MuleMessage muleMessage) {
-        def result = closure(this.dsqlQuery)
+        def result = closure(this.dSqlQuery)
         validateMockReturn(result)
         new DefaultMuleMessage(result,
                                this.muleContext)
@@ -41,14 +33,5 @@ class QueryTransformerSpy implements MuleMessageTransformer, SpyProcess {
             def item = result[0]
             throw new Exception("Must return a List<Map> result from your mock instead of List<${item.class}>!")
         }
-    }
-
-    void spy(MuleEvent muleEvent) throws MuleException {
-        def processor = locator.getProcessor(muleEvent) as QueryMessageProcessorDebuggable
-        def dsqlParserQuery = new DsqlMelParserUtils()
-        def prefixedDsql = dsqlParserQuery.parseDsql(this.muleContext,
-                                                     muleEvent,
-                                                     processor.query) as String
-        this.dsqlQuery = prefixedDsql.replace('dsql:', '')
     }
 }
