1a77b76ad0328f8582a54448e02dcfdfb7909e55
Brady Wied <brady@bswtechconsulting.com>
2018-08-07 15:19:50 -0600

    Just invoke flow directly

--- src/test/groovy/com/avioconsulting/mule/testing/invocation/SoapTest.groovy
+++ src/test/groovy/com/avioconsulting/mule/testing/invocation/SoapTest.groovy
@@ -2,8 +2,8 @@ package com.avioconsulting.mule.testing.invocation
 
 import com.avioconsulting.mule.testing.BaseTest
 import com.avioconsulting.mule.testing.OverrideConfigList
-import com.avioconsulting.schemas.soaptest.v1.SOAPTestRequestType
-import com.avioconsulting.schemas.soaptest.v1.SOAPTestResponseType
+import com.avioconsulting.mule.testing.soapxmlroot.SOAPTestRequest
+import com.avioconsulting.mule.testing.soapxmlroot.SOAPTestResponse
 import org.junit.Test
 
 import static org.hamcrest.Matchers.equalTo
@@ -18,21 +18,21 @@ class SoapTest extends BaseTest implements OverrideConfigList {
     @Test
     void input_output() {
         // arrange
-        def input = new SOAPTestRequestType().with {
+        def input = new SOAPTestRequest().with {
             title = 'hello there'
             approvalDate = getXmlDate(2018, 8, 07)
             it
         }
 
         // act
-        def result = runFlow('soap-api-main') {
+        def result = runFlow('/some/soap/flow') {
             soap {
                 inputPayload(input)
             }
-        } as SOAPTestResponseType
+        } as SOAPTestResponse
 
         // assert
         assertThat result.details,
-                   is(equalTo('howdy'))
+                   is(equalTo('theTitle hello there'))
     }
 }
