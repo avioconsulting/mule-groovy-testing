ad35af7c8c945eaea077f05acdf7ec0431733fce
Brady Wied <brady@bswtechconsulting.com>
2018-08-12 21:52:23 -0600

    Capture TODOs

--- src/main/groovy/org/mule/modules/interceptor/processors/MuleMessageTransformer.groovy
+++ src/main/groovy/org/mule/modules/interceptor/processors/MuleMessageTransformer.groovy
@@ -2,6 +2,7 @@ package org.mule.modules.interceptor.processors
 
 import org.mule.api.MuleMessage
 
+// TODO: Move to another package
 interface MuleMessageTransformer {
     MuleMessage transform(MuleMessage var1)
 }
--- src/main/groovy/org/mule/munit/common/mocking/SpyProcess.groovy
+++ src/main/groovy/org/mule/munit/common/mocking/SpyProcess.groovy
@@ -3,6 +3,7 @@ package org.mule.munit.common.mocking
 import org.mule.api.MuleEvent
 import org.mule.api.MuleException
 
+// TODO: Move to another package
 interface SpyProcess {
     void spy(MuleEvent var1) throws MuleException
 }
