9d34ec4ca80b39dd716ff877a4aa1e6bdcdb30c8
Brady Wied <brady@bswtechconsulting.com>
2018-08-13 11:26:05 -0600

    Add logged warnings

--- src/main/groovy/com/avioconsulting/mule/testing/mulereplacements/MockHandler.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/mulereplacements/MockHandler.groovy
@@ -1,5 +1,6 @@
 package com.avioconsulting.mule.testing.mulereplacements
 
+import groovy.util.logging.Log4j2
 import net.sf.cglib.proxy.MethodInterceptor
 import net.sf.cglib.proxy.MethodProxy
 import org.mule.api.AnnotatedObject
@@ -7,6 +8,7 @@ import org.mule.api.MuleEvent
 
 import java.lang.reflect.Method
 
+@Log4j2
 class MockHandler implements MethodInterceptor {
     private final MockingConfiguration mockingConfiguration
 
@@ -23,12 +25,20 @@ class MockHandler implements MethodInterceptor {
         if (method.name == 'process' &&
                 method.parameterTypes.size() == 1 &&
                 method.parameterTypes[0] == MuleEvent) {
-            def asAnnotated = obj as AnnotatedObject
-            def mock = mockingConfiguration.getMockProcess(asAnnotated)
-            def muleEvent = args[0] as MuleEvent
-            if (mock) {
-                return mock.process(muleEvent,
-                                    obj)
+            if (!(obj instanceof AnnotatedObject)) {
+                log.warn 'Processor {} does not implement AnnotatedObject so we cannot locate it for mocking',
+                         obj
+            } else if ((obj as AnnotatedObject).annotations.isEmpty()) {
+                log.warn 'Processor {} has zero annotations so we cannot locate it for mocking',
+                         obj
+            } else {
+                def asAnnotated = obj as AnnotatedObject
+                def mock = mockingConfiguration.getMockProcess(asAnnotated)
+                def muleEvent = args[0] as MuleEvent
+                if (mock) {
+                    return mock.process(muleEvent,
+                                        obj)
+                }
             }
         }
         return proxy.invokeSuper(obj, args)
