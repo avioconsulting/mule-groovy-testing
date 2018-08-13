e66ed3b616c657ccb02bd4df27bd4e718c79ac66
Brady Wied <brady@bswtechconsulting.com>
2018-08-13 00:20:55 -0600

    Work around prot methods

--- src/main/groovy/com/avioconsulting/mule/testing/mulereplacements/Handler.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/mulereplacements/Handler.groovy
@@ -3,18 +3,21 @@ package com.avioconsulting.mule.testing.mulereplacements
 import groovy.transform.Canonical
 import net.sf.cglib.proxy.MethodInterceptor
 import net.sf.cglib.proxy.MethodProxy
+import org.mule.api.processor.MessageProcessor
 
 import java.lang.reflect.Method
 
 @Canonical
 class Handler implements MethodInterceptor {
-    Object original
+    MessageProcessor processorWeMightMock
 
     @Override
     Object intercept(Object obj,
                      Method method,
                      Object[] args,
                      MethodProxy proxy) throws Throwable {
-        return proxy.invoke(original, args)
+        // work around protected methods for now
+        method.accessible = true
+        return method.invoke(processorWeMightMock, args)
     }
 }
