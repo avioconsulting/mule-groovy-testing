469545bf0eef33699109af7957574a9299d9eaf0
Brady Wied <brady@bswtechconsulting.com>
2018-08-13 13:36:20 -0600

    Remove stuff we don't need

--- src/main/groovy/com/avioconsulting/mule/testing/mulereplacements/SpyProcess.groovy
+++
@@ -1,9 +0,0 @@
-package com.avioconsulting.mule.testing.mulereplacements
-
-import org.mule.api.MuleEvent
-import org.mule.api.MuleException
-
-// TODO: Remove this one we're done converting
-interface SpyProcess {
-    void spy(MuleEvent var1) throws MuleException
-}
