b4400bb4551fb81339c3ebda1895e241b3941087
Brady Wied <brady@bswtechconsulting.com>
2018-08-13 01:01:41 -0600

    Capture

--- src/main/groovy/com/avioconsulting/mule/testing/mocks/HttpMock.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/mocks/HttpMock.groovy
@@ -4,6 +4,7 @@ import com.avioconsulting.mule.testing.mulereplacements.MockProcess
 import org.mule.api.MuleEvent
 import org.mule.module.http.internal.request.DefaultHttpRequester
 
+// TODO: Tie this in with the transformers
 class HttpMock implements MockProcess<DefaultHttpRequester> {
     @Override
     MuleEvent process(MuleEvent event,
