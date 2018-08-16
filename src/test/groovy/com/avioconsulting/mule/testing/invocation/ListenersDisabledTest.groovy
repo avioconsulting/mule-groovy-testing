f72019c48bb920e7fdd4e17b1a79435caadb5f0b
Brady Wied <brady@bswtechconsulting.com>
2018-08-16 12:30:59 -0600

    Don't use apikit for this

--- src/test/groovy/com/avioconsulting/mule/testing/invocation/ListenersDisabledTest.groovy
+++ src/test/groovy/com/avioconsulting/mule/testing/invocation/ListenersDisabledTest.groovy
@@ -1,6 +1,7 @@
 package com.avioconsulting.mule.testing.invocation
 
-import com.avioconsulting.mule.testing.BaseApiKitTest
+
+import com.avioconsulting.mule.testing.BaseJunitTest
 import com.avioconsulting.mule.testing.OverrideConfigList
 import groovy.util.logging.Log4j2
 import org.junit.Test
@@ -11,12 +12,43 @@ import static org.hamcrest.Matchers.is
 import static org.junit.Assert.assertThat
 
 @Log4j2
-class ListenersDisabledTest extends BaseApiKitTest implements OverrideConfigList {
+class ListenersDisabledTest extends BaseJunitTest implements OverrideConfigList {
+    private static final String TEST_PORT_PROPERTY = 'avio.test.http.port'
+
+    @Override
+    Properties getStartUpProperties() {
+        def properties = super.getStartUpProperties()
+        // have to have the listener running to use apikit
+        // http listener gets go
+        // ing before the properties object this method creates has had its values take effect
+        System.setProperty(TEST_PORT_PROPERTY,
+                           httpPort as String)
+        properties
+    }
+
+    static int getHttpPort() {
+        (8088..8199).find { candidate ->
+            try {
+                def socket = new ServerSocket(candidate)
+                socket.close()
+                true
+            }
+            catch (IOException ignored) {
+                false
+            }
+        }
+    }
+
+    static int getChosenHttpPort() {
+        // avoid duplicate ports
+        Integer.parseInt(System.getProperty(TEST_PORT_PROPERTY))
+    }
+
     @Test
     void does_not_listen() {
         // arrange
         def port = getChosenHttpPort()
-        def url = "http://localhost:${port}/the-app/api/v1/resources?foo=123&bar=456".toURL()
+        def url = "http://localhost:${port}/the-app/api/v1/howdy".toURL()
 
         // act
         log.info 'Attempting to access {}',
@@ -30,18 +62,11 @@ class ListenersDisabledTest extends BaseApiKitTest implements OverrideConfigList
                    is(instanceOf(String))
     }
 
-    String getApiNameUnderTest() {
-        'the-app'
-    }
-
-    String getApiVersionUnderTest() {
-        'v1'
-    }
-
+    @Override
     List<String> getConfigResourcesList() {
         [
                 'global-test.xml',
-                "${fullApiName}.xml".toString()
+                'listener-disabled-test.xml'
         ]
     }
 }
