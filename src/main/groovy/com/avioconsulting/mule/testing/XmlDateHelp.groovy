c57d5d2ee252cc79a1c08e0dad2a4201e8f78037
Brady Wied <brady@bswtechconsulting.com>
2018-08-17 11:22:10 -0600

    Add additional XML date helper

--- src/main/groovy/com/avioconsulting/mule/testing/XmlDateHelp.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/XmlDateHelp.groovy
@@ -2,6 +2,7 @@ package com.avioconsulting.mule.testing
 
 import javax.xml.datatype.DatatypeFactory
 import javax.xml.datatype.XMLGregorianCalendar
+import java.time.ZonedDateTime
 
 trait XmlDateHelp {
     XMLGregorianCalendar getXmlDate(int year, int oneBasedMonth, int dayOfMonth) {
@@ -15,6 +16,18 @@ trait XmlDateHelp {
         def zeroBasedMonth = oneBasedMonth - 1
         def gregorian = new GregorianCalendar(year, zeroBasedMonth, dayOfMonth, hourOfDay, minute, second)
         gregorian.setTimeZone(TimeZone.getTimeZone(timeZoneId))
+        DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorian)
+    }
+
+    XMLGregorianCalendar getXmlDateTime(ZonedDateTime dateTime) {
+        def zeroBasedMonth = dateTime.monthValue - 1
+        def gregorian = new GregorianCalendar(dateTime.year,
+                                              zeroBasedMonth,
+                                              dateTime.dayOfMonth,
+                                              dateTime.hour,
+                                              dateTime.minute,
+                                              dateTime.second)
+        gregorian.setTimeZone(TimeZone.getTimeZone(dateTime.zone))
         DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorian)
     }
 }
