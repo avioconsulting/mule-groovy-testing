4ecc85fb331fe29f5b8c9bd777476dbd0c683469
Brady Wied <brady@bswtechconsulting.com>
2018-08-22 11:21:32 -0600

    Add interface

--- src/main/groovy/com/avioconsulting/mule/testing/dsl/mocking/SOAPFormatter.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/dsl/mocking/SOAPFormatter.groovy
@@ -1,7 +1,13 @@
 package com.avioconsulting.mule.testing.dsl.mocking
 
+import javax.xml.namespace.QName
+
 interface SOAPFormatter extends XMLFormatter {
     def httpConnectError()
 
     def httpTimeoutError()
+
+    def soapFault(QName faultCode,
+                  QName subCode,
+                  Node detail)
 }
