bf12afd55f248982c26b34a630037375c13e928a
Brady Wied <brady@bswtechconsulting.com>
2018-09-17 17:40:33 -0600

    Document methods

--- src/main/groovy/com/avioconsulting/mule/testing/mulereplacements/MockingConfiguration.groovy
+++ src/main/groovy/com/avioconsulting/mule/testing/mulereplacements/MockingConfiguration.groovy
@@ -17,10 +17,17 @@ class MockingConfiguration {
         mocks[processorName] = mockHandler
     }
 
+    // consumed via reflection
     boolean isMocked(String connectorName) {
         mocks.containsKey(connectorName)
     }
 
+    /**
+     * consumed via reflection
+     *
+     * @param componentLocation - org.mule.runtime.api.component.location.ComponentLocation
+     * @param interceptionEvent - org.mule.runtime.api.interception.InterceptionEvent
+     */
     void executeMock(Object componentLocation,
                      Object interceptionEvent) {
         println 'our mock!'
