d8ee56c69fe5667650324dd6478baa08f697daaa
Brady Wied <brady@bswtechconsulting.com>
2018-09-27 13:12:24 -0600

    Code cleanup

--- src/test/groovy/com/avioconsulting/mule/testing/invocation/JavaTest.groovy
+++ src/test/groovy/com/avioconsulting/mule/testing/invocation/JavaTest.groovy
@@ -3,13 +3,14 @@ package com.avioconsulting.mule.testing.invocation
 import com.avioconsulting.mule.testing.OverrideConfigList
 import com.avioconsulting.mule.testing.junit.BaseJunitTest
 import org.junit.Test
-import org.mule.runtime.api.metadata.TypedValue
 
 import static org.hamcrest.Matchers.equalTo
 import static org.hamcrest.Matchers.is
 import static org.junit.Assert.assertThat
 
-class JavaTest extends BaseJunitTest implements OverrideConfigList {
+class JavaTest extends
+        BaseJunitTest implements
+        OverrideConfigList {
     List<String> getConfigResources() {
         ['java_test.xml']
     }
