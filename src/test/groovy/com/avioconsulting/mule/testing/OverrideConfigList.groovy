bc362ad910a635d366d95cb0d743732914b92ec8
Brady Wied <brady@bswtechconsulting.com>
2018-09-27 14:48:46 -0600

    Cache maven run to speed this up

--- src/test/groovy/com/avioconsulting/mule/testing/OverrideConfigList.groovy
+++ src/test/groovy/com/avioconsulting/mule/testing/OverrideConfigList.groovy
@@ -3,6 +3,8 @@ package com.avioconsulting.mule.testing
 import groovy.json.JsonSlurper
 import org.apache.commons.lang.SystemUtils
 
+import java.security.MessageDigest
+
 // allow us to avoid the overhead of setting up mule-deploy.properties for each test
 trait OverrideConfigList {
     static Map cachedClassLoaderModel
@@ -23,18 +25,33 @@ trait OverrideConfigList {
     Map getClassLoaderModel() {
         def logger = getLogger()
         if (!cachedClassLoaderModel) {
-            def mvnExecutable = SystemUtils.IS_OS_WINDOWS ? 'mvn.cmd' : 'mvn'
-            logger.info 'ClassLoader model has not been built yet, running {} against POM {} to generate one for testing the testing framework',
-                        mvnExecutable,
-                        mavenPomPath
-            def processBuilder = new ProcessBuilder(mvnExecutable,
-                                                    '-f',
-                                                    mavenPomPath.absolutePath,
-                                                    'clean',
-                                                    'compile')
-            def process = processBuilder.start()
-            process.inputStream.eachLine { println it }
-            assert process.waitFor() == 0
+            def digest = MessageDigest.getInstance('SHA-256')
+            digest.update(mavenPomPath.text.bytes)
+            def sha256 = Base64.encoder.encodeToString(digest.digest())
+            def digestFile = new File(testMavenDir, 'pom.xml.sha256')
+            def needUpdate = false
+            if (!digestFile.exists()) {
+                needUpdate = true
+            } else {
+                needUpdate = digestFile.text != sha256
+            }
+            if (needUpdate) {
+                def mvnExecutable = SystemUtils.IS_OS_WINDOWS ? 'mvn.cmd' : 'mvn'
+                logger.info 'ClassLoader model has not been built yet, running {} against POM {} to generate one for testing the testing framework',
+                            mvnExecutable,
+                            mavenPomPath
+                def processBuilder = new ProcessBuilder(mvnExecutable,
+                                                        '-f',
+                                                        mavenPomPath.absolutePath,
+                                                        'clean',
+                                                        'compile')
+                def process = processBuilder.start()
+                process.inputStream.eachLine { println it }
+                assert process.waitFor() == 0
+                digestFile.write(sha256)
+            } else {
+                logger.info 'Already up to date classLoader model'
+            }
             def classLoaderModelFile = new File(testMavenDir, 'target/META-INF/mule-artifact/classloader-model.json')
             assert classLoaderModelFile.exists()
             cachedClassLoaderModel = new JsonSlurper().parse(classLoaderModelFile)
