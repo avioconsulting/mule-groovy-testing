bcc873ed7d231887acae35e5c075dbf51ff120d8
Brady Wied <brady@bswtechconsulting.com>
2018-10-21 01:04:17 -0600

    Fix test script to use separate versions

--- src/test/integrationtest/runtest.sh
+++ src/test/integrationtest/runtest.sh
@@ -5,11 +5,12 @@ if [ "$VERSION" = "" ]
     exit 1
 fi
 
-set -e
+set -ex
 
 rm -rf temporary_repo
 ARTIFACT_DIR=temporary_repo/com/avioconsulting/mule/testing/$VERSION
 mkdir -p $ARTIFACT_DIR
 cp -v ../../../target/testing-$VERSION.jar $ARTIFACT_DIR
 cp -v ../../../pom.xml $ARTIFACT_DIR/testing-$VERSION.pom
-mvn -Dmaven.repo.local=temporary_repo -Dapp.runtime=$VERSION clean test
+MULE_VERSION=${VERSION:0:5}
+mvn -Dmaven.repo.local=temporary_repo -Dapp.runtime=$MULE_VERSION -Dtest.version=$VERSION clean test
