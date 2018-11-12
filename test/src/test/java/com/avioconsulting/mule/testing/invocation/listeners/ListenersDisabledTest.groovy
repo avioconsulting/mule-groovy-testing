6623cec7794fbf273081ad3104d03a4c13a92c9a
Brady Wied <brady@bswtechconsulting.com>
2018-11-11 17:15:09 -0700

    Not needed anymore
    Mule 4 doesn't seem to care if the port is occupied or not as long as no listeners are attached to a config. We already removed the port locator. Therefore probably no need for this

--- test/src/test/java/com/avioconsulting/mule/testing/invocation/listeners/ListenersDisabledTest.groovy
+++ test/src/test/java/com/avioconsulting/mule/testing/invocation/listeners/ListenersDisabledTest.groovy
@@ -3,14 +3,12 @@ package com.avioconsulting.mule.testing.invocation.listeners
 import com.avioconsulting.mule.testing.OverrideConfigList
 import com.avioconsulting.mule.testing.junit.BaseJunitTest
 import groovy.util.logging.Log4j2
-import org.junit.Ignore
 import org.junit.Test
 
 import static groovy.test.GroovyAssert.shouldFail
 import static org.hamcrest.Matchers.equalTo
 import static org.hamcrest.Matchers.is
 import static org.junit.Assert.assertThat
-import static org.junit.Assert.fail
 
 @Log4j2
 class ListenersDisabledTest extends
@@ -33,25 +31,6 @@ class ListenersDisabledTest extends
         // assert
         assertThat exception.message,
                    is(equalTo("Server returned HTTP response code: 503 for URL: http://localhost:${unusedPort}/the-app/api/v1/howdy".toString()))
-    }
-
-    // TODO: Look into disabling actual HTTP listener/global elements later on
-    @Test
-    @Ignore('MUnit does not disable the actual listener config/global element, just the flow listener. Will come back to this')
-    void port_not_used() {
-        // arrange
-        log.info 'Will attempt to bind to socket {}',
-                 unusedPort
-
-        try {
-            def server = new ServerSocket(unusedPort,
-                                          1,
-                                          InetAddress.loopbackAddress)
-            server.close()
-        }
-        catch (BindException e) {
-            fail('The loopback address/port should have been available for binding but it was not')
-        }
     }
 
     @Test
