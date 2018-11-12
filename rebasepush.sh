96f9333b8012fd8fcb2ed5b84595e5cfd1b8f48a
Brady Wied <brady@bswtechconsulting.com>
2018-11-12 10:54:55 -0700

    Add note

--- rebasepush.sh
+++ rebasepush.sh
@@ -20,3 +20,5 @@ git push --force dfw mule4_1/dfw:mule4_1/master
 git push --force origin mule4_1/dfw
 
 echo Now you can push to DFW Artifactory via Maven on your VPN VM and then switch back...
+echo Use the command 'mvn clean deploy -DskipTests'
+echo NOTE: DFW/VPN for some reason can block the maven deploy plugin from pushing a new version out. You may need to change the URL in implementation/pom.xml/distributionManagement to use localhost and SSH port forward to aodbprdc.dfw.dfwairport.com to complete the deploy
