3eb55b19f2a9b2de298a871454d5451d0d8c2801
Brady Wied <brady@bswtechconsulting.com>
2018-12-21 13:54:22 -0600

    Replicate problem with mock inside try

--- test/src/test/java/com/avioconsulting/mule/testing/mocking/ApiMockTest.groovy
+++ test/src/test/java/com/avioconsulting/mule/testing/mocking/ApiMockTest.groovy
@@ -146,6 +146,37 @@ class ApiMockTest extends
     }
 
     @Test
+    void mocks_get_try() {
+        // arrange
+        HttpRequesterInfo actualHttpRequestInfo = null
+        mockRestHttpCall('the name of our connector') {
+            json {
+                whenCalledWith(String) { HttpRequesterInfo httpInfo ->
+                    actualHttpRequestInfo = httpInfo
+                    'new payload'
+                }
+            }
+        }
+
+        // act
+        def result = runFlow('fooGetFlowTry') {
+            java {
+                inputPayload('nope')
+            }
+        }
+
+        // assert
+        assertThat result,
+                   is(equalTo('new payload'))
+        assertThat actualHttpRequestInfo.method,
+                   is(equalTo('GET'))
+        assertThat actualHttpRequestInfo.queryParams,
+                   is(equalTo([
+                           created_by: 'nope'
+                   ]))
+    }
+
+    @Test
     void mock_get_error() {
         // arrange
         mockRestHttpCall('the name of our connector') {
