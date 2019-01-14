0d0b683e5a3eab89a2db9b5793e046db3b5c2de8
Brady Wied <brady@bswtechconsulting.com>
2019-01-14 15:44:23 -0700

    Code cleanup/log

--- implementation/src/main/java/com/avioconsulting/mule/testing/transformers/http/SoapFaultTransformer.groovy
+++ implementation/src/main/java/com/avioconsulting/mule/testing/transformers/http/SoapFaultTransformer.groovy
@@ -73,15 +73,11 @@ class SoapFaultTransformer implements IHaveStateToReset,
             return event
         }
         if (connectorInfo.customHttpTransportConfigured) {
+            log.warn 'You are throwing a SOAP fault from a SOAP mock on a WSC config with a custom transport configured. When you have this configuration, Mule will treat the likely HTTP 500 coming back from the SOAP server as an exception and never get to the SOAP fault. The testing framework is mirroring this behavior so that you know it is happening.'
             // We have no easy way of getting a validator setup
             connectorInfo.validator.validate(500,
                                              'some fault',
                                              [:])
-//            def exception = new TestingFrameworkException('You are throwing a SOAP fault from a SOAP mock on a WSC config with a custom transport configured. When you have this configuration, Mule will treat the likely HTTP 500 coming back from the SOAP server as an exception and never get to the SOAP fault. The testing framework is mirroring this behavior so that you know it is happening.')
-//            log.error 'Halting mock execution',
-//                      exception
-//            throw exception
-
         }
         def detailResult = detailClosure(DOMBuilder.newInstance())
         def detailString = detailResult ? detailResult.serialize() : '<detail/>'
