1329566d106b7c52d10bd90f2609b1d97bc736fb
Brady Wied <brady@bswtechconsulting.com>
2019-05-15 21:04:06 -0600

    Pass the connect/timeout error cases

--- implementation/src/main/java/com/avioconsulting/mule/testing/muleinterfaces/wrappers/connectors/SoapConsumerInfo.groovy
+++ implementation/src/main/java/com/avioconsulting/mule/testing/muleinterfaces/wrappers/connectors/SoapConsumerInfo.groovy
@@ -1,7 +1,14 @@
 package com.avioconsulting.mule.testing.muleinterfaces.wrappers.connectors
 
 import com.avioconsulting.mule.testing.TestingFrameworkException
+import com.avioconsulting.mule.testing.dsl.mocking.SoapErrorThrowing
 import com.avioconsulting.mule.testing.muleinterfaces.wrappers.ConnectorInfo
+import com.avioconsulting.mule.testing.muleinterfaces.wrappers.CustomErrorWrapperException
+import com.avioconsulting.mule.testing.muleinterfaces.wrappers.EventWrapper
+import com.avioconsulting.mule.testing.transformers.ClosureEvaluationResponse
+
+import javax.xml.namespace.QName
+import java.util.concurrent.TimeoutException
 
 class SoapConsumerInfo extends
         ConnectorInfo implements HttpFunctionality {
@@ -10,6 +17,14 @@ class SoapConsumerInfo extends
     private final String uri
     private final String headers
     private HttpValidatorWrapper validatorWrapper
+    // all SOAP requests are POSTs
+    private static final String SOAP_METHOD = 'POST'
+    private final ClassLoader appClassLoader
+    @Lazy
+    private Class dispatchExceptionClass = {
+        appClassLoader.loadClass('org.mule.runtime.soap.api.exception.DispatchingException')
+    }()
+
 
     SoapConsumerInfo(String fileName,
                      Integer lineNumber,
@@ -22,6 +37,7 @@ class SoapConsumerInfo extends
         def connection = parameters['connection']
         def transportConfig = connection.transportConfiguration
         this.customTransport = transportConfig.getClass().getName().contains('CustomHttpTransportConfiguration')
+        this.appClassLoader = transportConfig.getClass().classLoader
         this.uri = connection.info.address
         this.headers = parameters['message'].headers?.text
         Object validator = null
@@ -96,5 +112,80 @@ class SoapConsumerInfo extends
 
     HttpValidatorWrapper getValidator() {
         this.validatorWrapper
+    }
+
+    private def throwConnectionException() {
+        Throwable exception
+        if (customHttpTransportConfigured) {
+            exception = getConnectionException(uri,
+                                               SOAP_METHOD,
+                                               appClassLoader)
+        } else {
+            exception = wrapWithCustom(dispatchExceptionClass.newInstance('An error occurred while sending the SOAP request') as Throwable)
+        }
+        throw exception
+    }
+
+    private def throwTimeoutException() {
+        Throwable exception
+        if (customHttpTransportConfigured) {
+            exception = getTimeoutException(uri,
+                                            SOAP_METHOD,
+                                            appClassLoader)
+        } else {
+            exception = wrapWithCustom(dispatchExceptionClass.newInstance('The SOAP request timed out',
+                                                                          new TimeoutException('HTTP timeout!')) as Throwable)
+        }
+        throw exception
+    }
+
+    private static Exception wrapWithCustom(Throwable dispatchException) {
+        // If you don't use the custom transport, all exceptions seem to have this type code
+        new CustomErrorWrapperException(dispatchException,
+                                        'WSC',
+                                        'CANNOT_DISPATCH')
+    }
+
+    @Override
+    ClosureEvaluationResponse evaluateClosure(EventWrapper event,
+                                              Object input,
+                                              Closure closure) {
+        def errorHandler = new SoapErrorThrowing() {
+            @Override
+            def soapFault(String message,
+                          QName faultCode,
+                          QName subCode) {
+                return null
+            }
+
+            @Override
+            def soapFault(String message,
+                          QName faultCode,
+                          QName subCode,
+                          Closure detailMarkupBuilderClosure) {
+                return null
+            }
+
+            // TODO: Better class structure/remove this
+            @Override
+            def setHttpStatusCode(int code) {
+                return null
+            }
+
+            @Override
+            def httpConnectError() {
+                throwConnectionException()
+            }
+
+            @Override
+            def httpTimeoutError() {
+                throwTimeoutException()
+            }
+        }
+        closure = closure.rehydrate(errorHandler,
+                                    closure.owner,
+                                    closure.thisObject)
+        def result = closure.parameterTypes.size() == 0 ? closure() : closure(input)
+        new ClosureEvaluationResponse(result)
     }
 }
