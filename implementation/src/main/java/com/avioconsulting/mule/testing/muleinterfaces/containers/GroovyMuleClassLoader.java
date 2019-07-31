c2b4d0e5ed1971f2f99a21f3f13ca0542d52b7fb
Brady Wied <brady@bswtechconsulting.com>
2019-07-30 20:39:10 -0600

    Explain (and be conservative about intercepting)

--- implementation/src/main/java/com/avioconsulting/mule/testing/muleinterfaces/containers/GroovyMuleClassLoader.java
+++ implementation/src/main/java/com/avioconsulting/mule/testing/muleinterfaces/containers/GroovyMuleClassLoader.java
@@ -11,9 +11,19 @@ public class GroovyMuleClassLoader extends URLClassLoader {
         super(urls, parent);
     }
 
+    private static boolean shouldInterceptExtensionSchemaGen(String name) {
+        String schemaGenProperty = System.getProperty("avio.groovy.test.generate.xml.schemas");
+        if (schemaGenProperty == null || schemaGenProperty.equals("false")) {
+            return false;
+        }
+        return name.equals("META-INF/services/org.mule.runtime.extension.api.dsl.syntax.resources.spi.ExtensionSchemaGenerator");
+    }
+
     @Override
     public Enumeration<URL> findResources(String name) throws IOException {
-        return name.equals("META-INF/services/org.mule.runtime.extension.api.dsl.syntax.resources.spi.ExtensionSchemaGenerator")
+        // allows us to influence how ServiceLoader finds the DefaultExtensionSchemaGenerator class
+        // we can put our own implementation in (if we want) and get access to the schemas
+        return shouldInterceptExtensionSchemaGen(name)
                 ? super.findResources("META-INF/ourschemagenoverride") :
                 super.findResources(name);
     }
