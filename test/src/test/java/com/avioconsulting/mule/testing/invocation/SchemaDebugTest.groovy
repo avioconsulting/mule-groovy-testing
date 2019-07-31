64f7b1d944377d0fb053e7bf74bf285aaeebb3e4
Brady Wied <brady@bswtechconsulting.com>
2019-07-30 20:50:21 -0600

    Add a test for this

---
+++ test/src/test/java/com/avioconsulting/mule/testing/invocation/SchemaDebugTest.groovy
@@ -0,0 +1,32 @@
+package com.avioconsulting.mule.testing.invocation
+
+import com.avioconsulting.mule.testing.ConfigTrait
+import com.avioconsulting.mule.testing.junit.BaseJunitTest
+import org.junit.Test
+
+import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.is
+import static org.junit.Assert.assertThat
+
+class SchemaDebugTest extends
+        BaseJunitTest implements
+        ConfigTrait {
+    List<String> getConfigResources() {
+        ['simple_json_test.xml']
+    }
+
+    @Override
+    boolean isGenerateXmlSchemas() {
+        true
+    }
+
+    @Test
+    void test() {
+        // arrange + act happen during engine start
+
+        // assert
+        def httpSchema = new File('.mule/schemas_from_testing_framework/mule-http.xsd')
+        assertThat httpSchema.exists(),
+                   is(equalTo(true))
+    }
+}
