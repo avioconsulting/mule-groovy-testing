5d2511858ab97ccd8b3a495e4e1e2ae6d5d2f9c1
Brady Wied <brady@bswtechconsulting.com>
2020-01-23 15:46:47 -0700

    4.2.2 brings a patch

--- test/src/test/java/com/avioconsulting/mule/testing/PatchesTest.groovy
+++ test/src/test/java/com/avioconsulting/mule/testing/PatchesTest.groovy
@@ -22,7 +22,10 @@ class PatchesTest extends
                                                               '**/*')
 
         // assert
-        assertThat serverPatches,
-                   is(equalTo([]))
+        assertThat serverPatches.size(),
+                   is(equalTo(1))
+        def patch = serverPatches[0]
+        assertThat patch,
+                   is(endsWith('MULE-17736-4.2.2-2.0.jar'))
     }
 }
