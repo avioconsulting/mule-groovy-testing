c185aa99bd1dd1213ddcfaf7a2ac2886112629ea
Brady Wied <brady@bswtechconsulting.com>
2020-03-28 00:25:01 -0600

    Keep track of the test objects we have

--- implementation/src/main/java/com/avioconsulting/mule/testing/background/ServerHandler.groovy
+++ implementation/src/main/java/com/avioconsulting/mule/testing/background/ServerHandler.groovy
@@ -7,15 +7,24 @@ import io.netty.channel.SimpleChannelInboundHandler
 import org.junit.runners.model.FrameworkMethod
 
 class ServerHandler extends SimpleChannelInboundHandler<String> {
+    private final Map<String, Object> testClasses = [:]
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx,
                                 String request) throws Exception {
         def close = false
         def parsedRequest = new JsonSlurper().parseText(request)
-        def testKlass = Class.forName(parsedRequest.klass)
-        def testMethod = testKlass.getMethod(parsedRequest.method)
+        def klassName = parsedRequest.klass as String
+        Object testObject
+        if (testClasses.containsKey(klassName)) {
+            testObject = testClasses[klassName]
+        } else {
+            def testKlass = Class.forName(klassName)
+            testObject = testKlass.newInstance()
+        }
+        def testMethod = testObject.class.getMethod(parsedRequest.method)
         def frameworkMethod = new FrameworkMethod(testMethod)
-        frameworkMethod.invokeExplosively(testKlass.newInstance())
+        frameworkMethod.invokeExplosively(testObject)
         def future = ctx.write('done\r\n')
         if (close) {
             future.addListener(ChannelFutureListener.CLOSE)
